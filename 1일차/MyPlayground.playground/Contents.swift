47bdabfa8da41c4af55f834206e8f68e8bdd8f51
JiHyeonMon <jhk8806@naver.com>
2022-01-24 23:16:59 +0900

    1일차

---
+++ 1일차/MyPlayground.playground/Contents.swift
@@ -0,0 +1,81 @@
+import Foundation
+
+/// 1, 상수와 변수
+///
+
+// 상수
+// let 상수명: 데이터 타입 = 값
+let a: Int = 100
+
+// 변수
+// var 변수명: 데이터 타입 = 값
+var b: Int = 200
+
+
+/// 2. 기본 데이터 타입
+///
+
+// Int
+var someInt: Int = 100
+
+// Unit
+var someUnit: UInt = 200  // -200 은 에러
+
+// Float
+var someFloat: Float = 1.1
+
+// Double
+var someDouble: Double = 0.5
+
+// Bool
+var someBool: Bool = true
+
+// Character
+var someChar: Character = "a"
+
+// String
+var someString: String = "abcde"
+
+// 타입추론
+var some = 100  // 컴파일러가 자동으로 some은 Int형으로 인식
+
+/// 3. 컬렉션 타입
+///
+// Array
+var numbers: Array<Int> = Array<Int>() // 빈 Int Array 생성
+// 데이터 추가
+numbers.append(1)
+numbers.append(2)
+// 데이터 접근
+numbers[0]
+// 데이터 특정 index에 추가
+numbers.insert(3, at: 2)
+// 데이터 삭제
+numbers.remove(at: 1)
+numbers
+ 
+// Dictionary
+//var dic: Dictionary<String, Int> = Dictionary<String, Int>()
+var dic:[String : Int] = ["one" : 1]
+dic["two"] = 2
+dic["zero"] = 0
+dic
+
+// 값 변경
+dic["zero"] = -1
+dic
+
+// 삭제
+dic.removeValue(forKey: "zero")
+dic
+
+// Set
+var set: Set = Set<Int>() // 축약형 선언없이 이 형태로만 사용할 수 있다.
+set.insert(10)
+set.insert(20)
+set.insert(20)
+set
+
+// 삭제
+set.remove(20)
+set
